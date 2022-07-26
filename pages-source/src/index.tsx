020becc0a47cceec81ecaea0e5e2717876c710d8
reayboi <brandonreay2000@gmail.com>
2022-07-26 10:48:31 +0000

    .

--- pages-source/src/index.tsx
+++ pages-source/src/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Home from "./Home";
 import reportWebVitals from "./reportWebVitals";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 

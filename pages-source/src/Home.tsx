02af19dd339c9a3cf6c15ecfb59f782cc824758f
reayboi <brandonreay2000@gmail.com>
2022-08-10 16:00:34 +0000

    pushing

--- pages-source/src/Home.tsx
+++ pages-source/src/Home.tsx
@@ -1,8 +1,18 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import "./App.css";
 
 function Home() {
-  return <div className="Home"></div>;
+  return (
+    <div className="Home">
+      <Typography variant="h2">
+        Hi, I'm <strong>Brandon Reay</strong>
+      </Typography>
+      <Typography variant="h5">
+        and I like to create things with <strong>code</strong>.
+      </Typography>
+    </div>
+  );
 }
 
 export default Home;
--- pages-source/src/index.tsx
+++ pages-source/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./Home";
 import reportWebVitals from "./reportWebVitals";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(

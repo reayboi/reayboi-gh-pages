ce85aeaa1ef3df3da808fb192353280de200514e
reayboi <brandonreay2000@gmail.com>
2022-08-12 10:12:50 +0100

    type-writer implemented

--- pages-source/src/Home.tsx
+++ pages-source/src/Home.tsx
@@ -1,16 +1,30 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 import "./App.css";
+import Typewriter from 'typewriter-effect';
 
 function Home() {
   return (
     <div className="Home">
-      <Typography variant="h2">
-        Hi, I'm <strong>Brandon Reay</strong>
-      </Typography>
-      <Typography variant="h5">
-        and I like to create things with <strong>code</strong>.
-      </Typography>
+      <Grid container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ minHeight: '100vh' }}>
+      <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString("Hi, <br />")
+      .pauseFor(300)
+      .typeString("I'm <strong>Brandon Reay</strong> and I like to create things with <strong>code</strong>.")
+      .pauseFor(2500)
+      .deleteAll()
+      .typeString("Welcome to <strong>reayboi.dev</strong>")
+      .start();
+  }}
+/>
+      </Grid>
+        
     </div>
   );
 }

97d87955259dd20afdd99a8af3ec5fb213f1e0bd
reayboi <brandonreay2000@gmail.com>
2023-01-30 22:08:07 +0000

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Home from "./Home";
-import reportWebVitals from "./reportWebVitals";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './Home';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
 );
-
-reportWebVitals();
